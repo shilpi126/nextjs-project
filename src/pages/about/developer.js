0a00a39c3b6ed5153d3e9958f0d7d47d5eed90a7
shilpi <shilpi8624@gmail.com>
2025-02-16 23:01:31 +0530

    :tada: creating nested route

---
+++ src/pages/about/developer.js
@@ -0,0 +1,9 @@
+
+const Developer = () => {
+    return (
+        <h1 className="text-2xl text-center mt-4">Developer Page</h1>
+        );
+}
+
+
+export default Developer
---
+++ src/pages/news.js
@@ -0,0 +1,9 @@
+
+const News = () => {
+    return (
+        <h1 className="text-2xl text-center mt-4">News Page</h1>
+        );
+}
+
+
+export default News
